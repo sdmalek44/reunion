d1f42e5c6eff77516a8e13370807f156f9eaa8ee
Stephen Malek <malek.stephen44@gmail.com>
2018-05-30 09:32:36 -0600

    add activity participants, calculate share

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,25 @@
 class Activity
+  attr_reader :name,
+              :participants,
+              :base_cost,
+              :cost_per_participant
+              
+  def initialize(name, base, per_participant)
+    @name = name
+    @participants = {}
+    @base_cost = base
+    @cost_per_participant = per_participant
+  end
+
+  def add_participants(participant, payment)
+    @participants[participant] = payment
+  end
+
+  def calculate_fair_share
+    total_cost = @base_cost + (@cost_per_participant * @participants.count)
+    total_cost / @participants.count
+  end
+
+
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,28 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    a = Activity.new('soccer', 200, 20)
+
+    assert_instance_of Activity, a
+  end
+
+  def test_it_takes_in_attributes
+    a = Activity.new('soccer', 200, 20)
+
+    assert_equal 'soccer', a.name
+    assert_equal 200, a.base_cost
+    assert_equal 20, a.cost_per_participant
+  end
+
+  def test_it_adds_participant
+    a = Activity.new('soccer', 200, 20)
+    expected_1 = {}
+    expected_2 = {'bob'=>0}
+
+    assert_equal expected_1, a.participants
+
+    a.add_participants('bob', 0)
+    assert_equal expected_2, a.participants
+  end
 end
